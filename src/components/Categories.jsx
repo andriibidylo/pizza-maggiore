3c1f803373be2f22ac9642ae3ce71e95b872493a
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-16 10:31:29 -0700

    Add Categories, Header, Sort, PizzaBlock components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+const Categories = () => {
+  return (
+    <div class="categories">
+    <ul>
+      <li class="active">All</li>
+      <li>Meat</li>
+      <li>Vegetarian</li>
+      <li>Grilled</li>
+      <li>Spicy</li>
+      <li>Closed</li>
+    </ul>
+  </div>
+  
+  )
+}
+
+export default Categories
