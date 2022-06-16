07e3715db35a480fc68377fc03b6fb7370925631
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-16 15:41:38 -0700

    render placholder and get items from mockapi

--- src/App.js
+++ src/App.js
@@ -4,29 +4,50 @@ import Categories from './components/Categories'
 import Sort from './components/Sort'
 import Header from './components/Header'
 import PizzaBlock from './components/PizzaBlock'
-import pizzas from './assets/pizzas.json'
 import Placeholder from './components/Placeholder'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsloading] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("https://62aba2a1bd0e5d29af136c7a.mockapi.io/itmes")
+        setItems(data)
+        setIsloading(false)
+      } catch (err) {
+        console.error(err)
+      }
+    })()
+  }, [])
+
+
   return (
     <div className="wrapper">
-    <Header/>
-    <div className="content">
+      <Header />
+      <div className="content">
         <div className="container">
           <div className="content__top">
-          <Categories/>
-          <Sort/>
-           </div>
+            <Categories />
+            <Sort />
+          </div>
           <h2 className="content__title">All pizzas</h2>
           <div className="content__items">
-          <Placeholder/>
-          {pizzas.map((obj)=> (
-            <PizzaBlock 
-            key={obj.id}
-            {...obj}
-            />
-          ))}
-         
+            {isLoading
+              ? [...new Array(9)].map((_, index) =>
+                <Placeholder key={index}
+                />
+              )
+              : items.map((obj) => (
+                <PizzaBlock
+                  key={obj.id}
+                  {...obj}
+                />
+              ))}
           </div>
         </div>
       </div>
