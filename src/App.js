76d54223642760e92ebf593c2516a5b6a681f211
andrewbidylo <77558037+andrewbidylo@users.noreply.github.com>
2022-06-21 15:04:20 -0700

    add main layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 
 import './scss/app.scss';
-import Header from './components/Header'
 import Home from './pages/Home'
 import Error from './pages/Error'
 import Cart from './pages/Cart'
 import ItemDetails from './pages/ItemDetails'
+import MainLayout from './layouts/MainLayout'
 import { Routes, Route } from 'react-router';
+
+
 
 const App = () => {
 
   return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/cart" exact element={<Cart />} />
-              <Route path="/pizza/:id" exact element={<ItemDetails />}/>
-              <Route path="*" exact element={<Error />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+    <Routes>
+      <Route path="/" exact element={<MainLayout />} >
+        <Route path="/" exact element={<Home />} />
+        <Route path="/cart" exact element={<Cart />} />
+        <Route path="/pizza/:id" exact element={<ItemDetails />} />
+        <Route path="*" exact element={<Error />} />
+      </Route>
+    </Routes>
   );
 }
 
